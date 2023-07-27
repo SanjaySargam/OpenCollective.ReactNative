836b4c22a7a6acacfbce9da11500d0a850719258
Sanjaykumar Sargam <sargamsanjaykumar@gmail.com>
2023-07-27 09:38:16 +0530

    used AysncStorage to store slug

---
+++ components/AsyncStorage.tsx
@@ -0,0 +1,19 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+  
+export const storeSlug = async (slug:string) => {
+    try {
+      await AsyncStorage.setItem("slug", JSON.stringify(slug));
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+export const getSlug = async () => {
+    try {
+      const savedUser = await AsyncStorage.getItem("slug");
+      const slug = JSON.parse(savedUser as string);
+      console.log("Slug",slug);
+    } catch (error) {
+      console.log(error);
+    }
+};
